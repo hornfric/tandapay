22724352f6ba2890c5d6ce71d8fd4c91c008d7a9
hornfricinc <corneliuskipkorir.dev@gmail.com>
2024-07-21 21:18:11 +0300

    The Latest Changes with the Jar

--- src/main/java/africa/tanda/tandaPay/daraja/service/B2CPaymentRequest.java
+++ src/main/java/africa/tanda/tandaPay/daraja/service/B2CPaymentRequest.java
@@ -1,6 +1,13 @@
 package africa.tanda.tandaPay.daraja.service;
 
+import org.json.JSONObject;
+
 public class B2CPaymentRequest {
 	
+	
+	
+	public JSONObject requestPaymentFromDarajaAPI(String jsonObject) {
+		
+	}
 
 }
